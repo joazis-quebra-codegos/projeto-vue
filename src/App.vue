0a786bf83a56e754d92ef483dee810bd1ccf5e74
joao_zamparetti <joao.ds.zantti@gmail.com>
2026-03-16 21:01:43 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Ola mundo projeto vue</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus, hic ad aliquam iusto quam neque incidunt quia atque, aliquid ex enim facilis mollitia consequuntur molestias, dicta eveniet error ipsum!</p>
+</template>
+
+<style>
+h1{
+  color: rgb(28, 28, 75);
+}
+
+p {
+  font-style: italic;
+}
+
+</style>
+
+
