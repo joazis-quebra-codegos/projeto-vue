f97055f2f66b4abe4f472ded78816544b1db66df
joao_zamparetti <joao.ds.zantti@gmail.com>
2026-03-16 21:40:20 -0300

    nome idade e filme no App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+const cidade = "Tubarão, SC"
+const escola = "Senai"
+const nome = "Nome genérico"
+const idade = "Idade genérica"
+const filme = "Filme 1 2 e guardiões da galáxia"
 </script>
 
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
-          <h1>Ola mundo projeto vue</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus, hic ad aliquam iusto quam neque incidunt quia atque, aliquid ex enim facilis mollitia consequuntur molestias, dicta eveniet error ipsum!</p>
+        <h1>Ola mundo projeto vue {{ cidade }} {{ escola }}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur necessitatibus, hic ad aliquam iusto quam neque incidunt quia atque, aliquid ex enim facilis mollitia consequuntur molestias, dicta eveniet error ipsum!</p>
+        <h4>{{ nome }} {{ idade }} {{ filme }}</h4>
       </div>
     </div>
   </div>
